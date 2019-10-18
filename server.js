0c14334068ab3c0f64c2f2f518fddf5288da63c4
Daniel Chiu <cycdaniel@gmail.com>
2019-10-18 02:30:50 -0500

    Fixed app.engine

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: main }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
